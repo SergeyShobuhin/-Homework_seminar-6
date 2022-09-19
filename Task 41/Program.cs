cc4a8e0543a211c8b12999e1277a30db82802bfd
Sergey Shobuhin <shobuhin@mail.ru>
2022-09-19 20:46:07 +0700

    Задача 41,43

---
+++ Task 41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int count = 0; 
+
+// функция расчёта > 0
+int NumberGreaterZero(int [] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    count++;
+                }
+            }
+
+        return count;        
+    }
+
+
+Console.Write("Введите чисела через пробел или запятую: "); // Ввод чисел
+var array = Array.ConvertAll(Console.ReadLine().Split(' ', '.', ','), int.Parse); // отсекаем пробелы запятые и точки
+
+NumberGreaterZero(array); // вызываем функцию
+
+Console.WriteLine($"Чисел больше 0 = {count}"); // выводим результат
+
---
+++ Task 43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+//функция вычесления
+string GetIntersectionPoint(double b1, double k1, double b2, double k2)
+    {
+        string rez = "прямые не пересекаются ";
+        double x = (-b2 + b1)/(-k1 + k2);
+        double y = k2 * x + b2;
+        string rezult = $"прямые пересекаются в точке: ({Math.Round(x, 2)} ; {Math.Round(y, 2)})";
+                    
+        return rezult;
+        }
+    
+// Ввод чисел
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+//вызов функции
+var checkPoint = GetIntersectionPoint(b1,k1,b2,k2);
+
+//вывод значения
+Console.WriteLine(checkPoint);
